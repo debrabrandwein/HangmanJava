c23cf9d8db58c34b5d90e65972fd107596fb9752
Debra Brandwein <deb@Debra-Brandweins-MacBook-Pro.local>
2014-09-24 11:57:05 -0700

    Very basic set a and e

--- src/main/java/com/hangman/players/YourPlayer.java
+++ src/main/java/com/hangman/players/YourPlayer.java
@@ -1,4 +1,6 @@
 package com.hangman.players;
+import com.hangman.Game;
+import com.hangman.HangmanGame;
 import com.hangman.Player;
 
 import java.util.Arrays;
@@ -6,8 +8,36 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class YourPlayer implements Player {
+
+    private List<Character> myGuesses = new LinkedList<Character>();
+    private List<Character> clue;
+
   @Override
   public char GetGuess(List<Character> clue) {
-    return 'a';
+      this.clue = clue;
+      int missing =  lettersMissing(clue);
+
+      if (myGuesses.size() == 0 ) {
+          myGuesses.add('a');
+          return 'a';
+      }
+
+      return 'e';
+
   }
+
+    public int lettersMissing(List<Character> clue ) {
+        int letters = 0;
+        for (char missing: clue ) {
+            letters += (missing == '_')?1:0;
+        }
+
+        return letters;
+
+    }
+
+    public boolean validLetter( char letter ) {
+        return this.clue.contains( letter );
+    }
+
 }
--- src/test/java/com/hangman/players/YourPlayerTest.java
+++ src/test/java/com/hangman/players/YourPlayerTest.java
@@ -1,16 +1,45 @@
 package com.hangman.players;
 
+import com.hangman.FakeGame;
+import org.junit.Before;
 import org.junit.Test;
 import java.util.Arrays;
 import static org.junit.Assert.assertEquals;
 
 public class YourPlayerTest {
+
+    YourPlayer player;
+
+    @Before
+    public void init() {
+        player = new YourPlayer();
+    }
+
     @Test
     public void AlwaysGuessA() {
-        YourPlayer player = new YourPlayer();
 
         char guess = player.GetGuess(Arrays.asList('a', 'b', 'c'));
 
         assertEquals('a', guess);
     }
+
+    @Test
+    public void HowManyLettersMissing() {
+        int missing = player.lettersMissing(Arrays.asList('_','_','a','_'));
+
+        assertEquals(3,missing);
+    }
+
+    @Test
+    public void DidAGuessSucceed() {
+
+        player.GetGuess(Arrays.asList('_', '_', '_'));
+
+        player.GetGuess(Arrays.asList('a', '_', '_'));
+
+        boolean success = player.validLetter('a');
+
+        assertEquals(true, success);
+    }
+
 }
